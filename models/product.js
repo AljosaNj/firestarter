2c84e9af2f53ac5ed9dfc5b7a3d39b5e5aed492d
AljosaNj <aljosaa.njezic@gmail.com>
2022-09-05 18:22:33 +0200

    first deploy

---
+++ models/product.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+
+ name: {
+  type:String,
+  required: [true,'product name must be provided']
+},
+price: {
+ type:Number,
+ required: [true,'product price must be provided']
+}
+,featured: {
+type:Boolean,
+default:false
+},
+
+description: {
+type: String,
+required: [true,'Please provide product description'],
+maxlength: [1000,'Description can not be more than 1000 characters'],
+},
+image: {
+ type: String,
+ //default: '/uploads/example.jpeg',
+},
+
+
+
+ rating: {
+  type:Number,
+  default: 4.5,
+ },
+ createdAt: {
+  type:Date,
+  default: Date.now(),
+ },
+ company: {
+  type:String,
+  enum:{
+  values: ['ikea','liddy','caressa','marcos'],
+  message: '{VALUE} is not supported',
+  },
+  
+ },
+})
+
+
+
+module.exports = mongoose.model('Product',productSchema)
